72465ff291ce2bbd20eafda03d6c8004a6796eb5
Luca Canali <info@canaliluca.com>
2019-09-18 11:50:37 +0200

    clean file

--- clearfolder.py
+++ clearfolder.py
@@ -1,11 +1,11 @@
-import time
-import datetime
+from time import sleep, strftime
+from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import os
+from os import mkdir, path, rename, stat
 from ntpath import basename
 import filetype
-
+from filetype import guess_extension, guess_mime
 
 class Watcher:
     DIRECTORY_TO_WATCH = "/home/luca/prova/"
@@ -19,7 +19,7 @@ class Watcher:
         self.observer.start()
         try:
             while True:
-                time.sleep(5)
+                sleep(5)
         except KeyboardInterrupt:
             self.observer.stop()
 
@@ -40,8 +40,8 @@ class Handler(FileSystemEventHandler):
             # Take any action here when a file is first created.
             print(f"Received created event  {filename}")
             # Get file extension, mime and more
-            file_guess_mime = filetype.guess_mime(filename)
-            file_guess_extension = filetype.guess_extension(filename)
+            file_guess_mime = guess_mime(filename)
+            file_guess_extension = guess_extension(filename)
             # Set filetype if not match set to None
             if file_guess_mime is None:
                 file_type = "None"
@@ -61,30 +61,29 @@ class Handler(FileSystemEventHandler):
                 else:
                     file_extension = filename_split.append(file_guess_extension)
             # Get current file Year
-            modification_time = os.stat(filename).st_mtime
-            # year = time.strftime("%Y") # Use this line if you want a current year
-            year = datetime.datetime.fromtimestamp(modification_time).strftime('%Y')
+            modification_time = stat(filename).st_mtime
+            # year = strftime("%Y") # Use this line if you want a current year
+            year = datetime.fromtimestamp(modification_time).strftime('%Y')
             # Get current file Month
-            # month = time.strftime("%m") # Use this line if you want a current month
-            month = datetime.datetime.fromtimestamp(modification_time).strftime('%m')
+            # month = strftime("%m") # Use this line if you want a current month
+            month = datetime.fromtimestamp(modification_time).strftime('%m')
 
-            if not os.path.exists(destination + "/" + year):
-                os.mkdir(destination + "/" + year)
-            if not os.path.exists(destination + "/" + year + "/" + month):
-                os.mkdir(destination + "/" + year + "/" + month)
-            if not os.path.exists(destination + "/" + year + "/" + month + "/" + file_type):
-                os.mkdir(destination + "/" + year + "/" + month + "/" + file_type)
+            if not path.exists(destination + "/" + year):
+                mkdir(destination + "/" + year)
+            if not path.exists(destination + "/" + year + "/" + month):
+                mkdir(destination + "/" + year + "/" + month)
+            if not path.exists(destination + "/" + year + "/" + month + "/" + file_type):
+                mkdir(destination + "/" + year + "/" + month + "/" + file_type)
 
             destination = destination + "/" + year + "/" + month + "/" + file_type
 
             new_name = destination + "/" + filename_split[0] + "." + file_extension
             i = 0
-            while os.path.exists(new_name):
+            while path.exists(new_name):
                 i += 1
                 new_name = destination + "/" + filename_split[0] + str(i) + "." + file_extension
                 print(new_name)
             # Move file event.src_path in to destination ex. {year}/{month}/{basename(event.src_path)}
-            os.rename(filename, new_name)
 
 
 if __name__ == '__main__':
