d846f676233836b7cde250726b1137e447e09f78
Luca Canali <info@canaliluca.com>
2019-09-19 16:38:43 +0200

    Version 0.1 is done

--- clearfolder.py
+++ clearfolder.py
@@ -1,24 +1,67 @@
+"""
+    Name: clearfolder.py
+    Version: 0.1
+    Description: This script used to move every file in a specified direcoty sored by year, month and type
+        ex. /home/user/Storage/2019/09/Image/file.png
+            /home/user/Storage/2019/09/Document/file.pdf
+
+    Copyright (C) 2019 Luca Canali
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from time import sleep, strftime
 from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from os import mkdir, path, rename, stat
 from ntpath import basename
-import filetype
 from filetype import guess_extension, guess_mime
+from sys import argv
+from pathlib import Path
+
+directory_to_watch = ""
+try:
+    if 1 < len(argv) <= 2:
+        # Check how many parameters are passed
+        if path.isdir(argv[1]):
+            # Check if parameter passed is a directory
+            directory_to_watch = argv[1] + "/"
+        else:
+            print("The path isn't exist or isn't a directory")
+            quit()
+    else:
+        print("The parameters is too much or too few")
+        quit()
+except IndexError:
+    print("No parameters passed")
+    quit()
+
 
 class Watcher:
-    DIRECTORY_TO_WATCH = "/home/luca/prova/"
 
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
         event_handler = Handler()
-        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=False)
+        self.observer.schedule(event_handler, directory_to_watch, recursive=False)
         self.observer.start()
+
         try:
             while True:
+                # Check every 5 seconds
                 sleep(5)
         except KeyboardInterrupt:
             self.observer.stop()
@@ -28,46 +71,52 @@ class Watcher:
 
 class Handler(FileSystemEventHandler):
 
+    # Get only event
     @staticmethod
     def on_any_event(event):
-        destination = "/home/luca/prova/dest/"
         filename = event.src_path
-
         if event.is_directory:
             return None
-
         elif event.event_type == 'created':
-            # Take any action here when a file is first created.
-            print(f"Received created event  {filename}")
-            # Get file extension, mime and more
+            # Take event created
+            destination = directory_to_watch + "Storage/"
+            if not path.exists(destination):
+                # Make destination if not exists
+                mkdir(destination)
+            # Get file extension and mime
             file_guess_mime = guess_mime(filename)
             file_guess_extension = guess_extension(filename)
-            # Set filetype if not match set to None
             if file_guess_mime is None:
+                # Set filetype None if doesn't match mime
                 file_type = "None"
             else:
+                # Set filetype to the matched mime
                 file_guess_mime_split = file_guess_mime.split("/")
                 file_type = str(file_guess_mime_split[0]).capitalize()
-            # Separate filename to extension if non extension put extension from filetype
             filename_split = basename(filename).split(".")
             try:
+                # If file have extension
                 if len(filename_split) > 1:
+                    # If filename have multiple dot in the name get only the last
                     file_extension = filename_split[-1]
                 else:
+                    # Else get second
                     file_extension = filename_split[1]
             except IndexError:
-                if file_extension is None:
-                    file_extension = filename_split.append("noname")
+                if file_guess_extension is None:
+                    # If file haven't extension set to noname
+                    file_extension = "noname"
                 else:
+                    # Else get extension from guess_extension
                     file_extension = filename_split.append(file_guess_extension)
-            # Get current file Year
+            # Get modification time of filename
             modification_time = stat(filename).st_mtime
-            # year = strftime("%Y") # Use this line if you want a current year
+            # Set Year with modification time
             year = datetime.fromtimestamp(modification_time).strftime('%Y')
-            # Get current file Month
-            # month = strftime("%m") # Use this line if you want a current month
+            # Set Month with modification time
             month = datetime.fromtimestamp(modification_time).strftime('%m')
 
+            # Check if the directories Storage, Year and Month exists if they don't exists create them
             if not path.exists(destination + "/" + year):
                 mkdir(destination + "/" + year)
             if not path.exists(destination + "/" + year + "/" + month):
@@ -75,15 +124,18 @@ class Handler(FileSystemEventHandler):
             if not path.exists(destination + "/" + year + "/" + month + "/" + file_type):
                 mkdir(destination + "/" + year + "/" + month + "/" + file_type)
 
+            # Set final destination to the path made before
             destination = destination + "/" + year + "/" + month + "/" + file_type
-
+            # Set final file name and destinations
             new_name = destination + "/" + filename_split[0] + "." + file_extension
             i = 0
             while path.exists(new_name):
+                # If new_name is an existing file add a number at the name
                 i += 1
                 new_name = destination + "/" + filename_split[0] + str(i) + "." + file_extension
                 print(new_name)
-            # Move file event.src_path in to destination ex. {year}/{month}/{basename(event.src_path)}
+            # Move the file to the destinations directory
+            rename(filename, new_name)
 
 
 if __name__ == '__main__':
